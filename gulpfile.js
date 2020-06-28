3b964ec3ebb794160ef054986479233a0db9c81d
Corey Vittitow <corey.vittitow@e-farmcredit.com>
2020-06-28 10:18:25 -0400

    Initial site.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+const purgecss = require('gulp-purgecss');
+
+gulp.task('css:dev', () => {
+    return gulp.src('./site.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('css:prod', () => {
+    return gulp.src('./site.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([
+            require('precss'),
+            require('tailwindcss'),
+            require('autoprefixer')
+        ]))
+        .pipe(purgecss({ content: ['**/*.html'] }))
+        .pipe(cleanCSS({ level: 2 }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css/'));
+});
